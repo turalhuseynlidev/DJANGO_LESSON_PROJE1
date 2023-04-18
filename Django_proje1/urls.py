d9dc697d75d357127debd659c331f245d821a11f
Tural Huseynli <tural.huseynli91@outlook.com>
2023-04-18 11:39:36 +0400

    Django template

--- Django_proje1/urls.py
+++ Django_proje1/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from page.views import home
+from page.views import home, about_us, contact #aghamir
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('haqqimizda/', about_us),
+    path('contact/', contact),
+   
+    # path('staff/aghamir_aghamirli', aghamir),
     
 ]
+
--- page/views.py
+++ page/views.py
@@ -5,4 +5,17 @@ from django.http import HttpResponse
 def home(request):
     #print("request:::", request.META)
     #print("request:::", request.HEADERS)
-    return render(request, 'page/homepage.html', {})
+    context = dict()
+    return render(request, 'page/homepage.html', context)
+
+def about_us(request):
+    context = dict()
+    return render(request, "page/about_us.html", context)
+
+def contact(request):
+    context = dict()
+    return render(request, "page/contact.html", context)
+
+# def aghamir(request):
+#     context = dict()
+#     return HttpResponse('Agamir Agamirli.')
